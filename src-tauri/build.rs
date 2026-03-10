783a82fc0e12f7526ccdd442980ecc9be32113a5
jhonny <giovanni.forlenza.gf@gmail.com>
2026-03-10 23:25:13 +0100

    Remove macOS-specific ScreenCaptureKit build configuration from build.rs

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,15 +1,3 @@
 fn main() {
-    #[cfg(target_os = "macos")]
-    {
-        println!("cargo:rustc-link-search=framework=/System/Library/PrivateFrameworks");
-        println!("cargo:rustc-link-lib=framework=ScreenCaptureKit");
-
-        cc::Build::new()
-            .file("src/capture.m")
-            .flag("-fobjc-arc")
-            .flag("-fmodules")
-            .compile("capture");
-    }
-
     tauri_build::build()
 }
